0a2b761da40106460deb0d306b8580ed8fc4c29e
Andrei Mesén Romero <mrandrei.dev@gmail.com>
2026-06-23 21:09:20 -0600

    La app aranca y muestra 'Tomcat started on port 8080'.

---
+++ src/main/java/com/laboratorio/laboratorio_cruds/LaboratorioCrudsApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.laboratorio_cruds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioCrudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioCrudsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratorio/laboratorio_cruds/LaboratorioCrudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratorio.laboratorio_cruds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioCrudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
